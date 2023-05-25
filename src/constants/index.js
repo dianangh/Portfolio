bb8e4318f30297d486fa85ec20b32c8273650496
dianangh <dianangh@gmail.com>
2023-05-25 12:41:59 -0500

    added constant, hoc, utils directories

---
+++ src/constants/index.js
@@ -0,0 +1,313 @@
+import {
+	developer,
+	backend1,
+	frontend,
+	javascript,
+	python,
+	html,
+	css,
+	reactjs,
+	fastapi,
+	postgresql,
+	redux,
+	tailwind,
+	bootstrap,
+	mongodb,
+	docker,
+	django,
+	automatepro,
+	threejs,
+	wardrobe,
+	caloriesplanner,
+	git,
+	sqlite3,
+	vite,
+	mysql,
+	emily,
+	mico,
+	paula,
+	shaun,
+	sophia,
+	brian,
+	ali,
+	abdalla,
+	adrian,
+} from "../assets";
+
+export const navLinks = [
+	{
+		id: "about",
+		title: "About",
+	},
+	{
+		id: "tech",
+		title: "Technologies",
+	},
+	{
+		id: "projects",
+		title: "Projects",
+	},
+	{
+		id: "feedback",
+		title: "Recommendations",
+	},
+	{
+		id: "contact",
+		title: "Contact",
+	},
+];
+
+export const services = [
+	{
+		title: "Full-Stack Web Developer",
+		icon: developer,
+	},
+	{
+		title: "Frontend Developer",
+		icon: frontend,
+	},
+	{
+		title: "Backend Developer",
+		icon: backend1,
+	},
+];
+
+export const languagesTech = [
+	{
+		name: "HTML",
+		icon: html,
+	},
+	{
+		name: "CSS",
+		icon: css,
+	},
+	{
+		name: "JavaScript",
+		icon: javascript,
+	},
+	{
+		name: "Python",
+		icon: python,
+	},
+];
+
+export const frontEndTech = [
+	{
+		name: "React",
+		icon: reactjs,
+	},
+	{
+		name: "Redux",
+		icon: redux,
+	},
+	{
+		name: "Tailwind",
+		icon: tailwind,
+	},
+	{
+		name: "Bootstrap",
+		icon: bootstrap,
+	},
+	{
+		name: "Three",
+		icon: threejs,
+	},
+];
+
+export const backEndTech = [
+	{
+		name: "Django",
+		icon: django,
+	},
+	{
+		name: "FastAPI",
+		icon: fastapi,
+	},
+	{
+		name: "MongoDB",
+		icon: mongodb,
+	},
+	{
+		name: "MySql",
+		icon: mysql,
+	},
+	{
+		name: "SQLite3",
+		icon: sqlite3,
+	},
+	{
+		name: "PostgreSQL",
+		icon: postgresql,
+	},
+];
+
+export const toolsTech = [
+	{
+		name: "Git",
+		icon: git,
+	},
+	{
+		name: "Docker",
+		icon: docker,
+	},
+	{
+		name: "Vite",
+		icon: vite,
+	},
+];
+
+export const recommendations = [
+	{
+		recommendation:
+			"Diana and I were able to work together on one of our paired projects during Hack Reactor's Software Immersive program, and I felt very lucky to get to collaborate with Diana. Diana has a great work ethic and is calm but persistent when debugging and problem-solving. She is quite organized with great time management skills that help keep her team members on track with completing tasks and meeting deadlines. Diana's exceptional communication skills and positive attitude make her a pleasure to work with, especially in the face of challenges. I have no doubt that Diana will continue to excel in her journey as a software engineer. Any team would be lucky to have Diana as a member.",
+		name: "Emily Arai",
+		designation: "Software Engineer",
+		image: "../assets/linkedin/emily.jpeg",
+		linkedin: "https://www.linkedin.com/in/emily-arai/",
+	},
+	{
+		recommendation:
+			"Diana is an asset to any team. She is smart, curious, and humble which makes her able to always learn from others without any ego. She is also incredibly generous and patient to teach and share any knowledge and resources she can provide others. She is a tenacious problem solver, with a keen eye for spotting a bug. It has been such a pleasure working alongside her. She is kind, driven, and incredibly hard-working. She inspires me to do my best work every day.",
+		name: "Paula Mejia",
+		designation: "Full Stack Software Engineer",
+		image: {paula},
+		linkedin: "https://www.linkedin.com/in/paula-natalia-mejia/",
+	},
+	{
+		recommendation:
+			"During our time together at Hack Reactor's Software Engineering Immersive program, I witnessed first-hand her fervor for improving herself by relentlessly tackling new concepts and technologies with confidence. Her ability to always find new and interesting questions about the topics at hand demonstrates her creative thinking and her capacity to grow. As a collaborator, she always finds time to share useful and unique resources that benefits the entire group. She's responsible with her time and priorities, and always aims to deliver the highest quality work. I have zero doubts that any future team she joins will be grateful for what she brings to the table.",
+		name: "Shaun Ragasa",
+		designation: "Full Stack Software Engineer",
+		image: {shaun},
+		linkedin: "https://www.linkedin.com/in/shaunragasa/",
+	},
+	// {
+	// 	recommendation:
+	// 		"I had the pleasure of working with Diana on a full-stack web application during our time at Hack Reactor. She was a good communicator and a great problem-solver when it came to debugging code. Her collaboration skills and attention to detail were invaluable. Diana is always willing to go above and beyond to ensure the success of a project. She always maintained a cheerful attitude and a strong work ethic. Diana will be more than capable of completing any project or job she is considering.",
+	// 	name: "Adrian Olivares",
+	// 	designation: "Full Stack Engineer",
+	// 	image:
+	// 		"",
+	// 	linkedin: "https://www.linkedin.com/in/adrian-olivares-cruz/",
+	// },
+	// {
+	// 	recommendation:
+	// 		"I had the pleasure of working with Diana who is a talented developer with a strong drive to understand everything she works on. She communicates her ideas clearly and effectively and responded to criticism with open arms. She's incredibly hardworking and has a serious nack for design. I highly recommend Diana not only as a developer but an amazing teammate to work with who holds responsibility for her work. She's also incredibly kind, and anyone would be lucky to have her on their team!",
+	// 	name: "Mico Hernandez",
+	// 	designation: "Full Stack Software Engineer",
+	// 	image:
+	// 		"https://media.licdn.com/dms/image/D4E03AQGO9lGWKmfKlg/profile-displayphoto-shrink_400_400/0/1673649686721?e=1687392000&v=beta&t=NxQuRM8AupVig_CzbDwZ1fKtMBrkOkP-8t3QBSkNSUs",
+	// 	linkedin: "https://www.linkedin.com/in/mico-hernandez/",
+	// },
+	// {
+	// 	recommendation:
+	// 		"Diana was my junior at HackReactor and she showed great curiosity and willingness to learn. The program is mentally grueling and time consuming, but she put in the hours above and beyond to create full-stack personal projects. Diana will be a great resource for any team who gives her the chance to thrive.",
+	// 	name: "Sophia Hu",
+	// 	designation: "Full Stack Engineer",
+	// 	image:
+	// 		"https://media.licdn.com/dms/image/D5603AQHJF7saTdGfUg/profile-displayphoto-shrink_400_400/0/1668473160094?e=1687392000&v=beta&t=2hdjipg__w3GvjvkBtH34i6l_ztowthkLtEuYVSKr3Q",
+	// 	linkedin: "https://www.linkedin.com/in/super-sophia-hu/",
+	// },
+	// {
+	// 	recommendation:
+	// 		"I had the pleasure of working with Diana as part of a team of 5 for our Hack Reactor Advanced Software Engineering Certification final project where we developed a full-stack calorie tracker app. Diana consistently gave 100% effort to the team by taking responsibility and initiative on various app component designs and through effective communication. She took the time to help team-members along the way and be involved with issue tracking, making sure we all succeed together. I would love to work with Diana again and would highly recommend her for her excellent communication skills, teamwork capabilities, and technical skills in software development.",
+	// 	name: "Ali Bhatti",
+	// 	designation: "Full Stack Engineer",
+	// 	image:
+	// 		"https://media.licdn.com/dms/image/D5603AQGrRHkJ4zMv5w/profile-displayphoto-shrink_200_200/0/1676561240240?e=1688601600&v=beta&t=7g-5EylC7WjgjxzFn8ZCllD9QTh0KTyooI1YTlL6x2w",
+	// 	linkedin: "https://www.linkedin.com/in/malibhatti/",
+	// },
+];
+
+
+export const projects = [
+	{
+		name: "AutoMatePro",
+		description:
+			"application designed to efficiently manage inventory, service center, and sales using the Django MVC framework. Enjoy a user-friendly experience with React components, which include features like form submission and search/filter.",
+			// "Application that helps automobile dealerships streamline their operations by offering a microservices-based solution that enables efficient inventory management, service center, and sales management. This application is designed and engineered using a Django MVC framework, which allows for creating efficient RESTful APIs for the back end. In addition, using React components on the front end resulted in a user-friendly experience, with convenient features such as form submission and search/filter components.",
+		tags: [
+			{
+				name: "react",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "postgres",
+				color: "green-text-gradient",
+			},
+			{
+				name: "Bootstrap",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "Django",
+				color: "orange-text-gradient",
+			},
+		],
+		image: automatepro,
+		source_code_link: "https://gitlab.com/dangaritaho/project-beta",
+	},
+	{
+		name: "Wardrobe Wizards",
+		description:
+			"The Wardrobe Wizards application is designed for individuals with extensive wardrobes who struggle to organize their hats and shoes. This application employs a microservices architecture and RESTful APIs to facilitate efficient communication among all its different components.",
+			// "The Wardrobe Wizards application was created specifically for those with a large wardrobe struggling to organize their hats and shoes. The application utilizes a microservices architecture and RESTful APIs, which work together seamlessly to ensure all the various components of the application communicate effectively. In addition, the application's user-friendly React front end provides an easy-to-use interface for users to navigate through their inventory, allowing them to manage their collection efficiently. Overall, the Wardrobe Wizards application is an excellent example of how technology can be used to simplify everyday tasks and make life a little bit easier.",
+		tags: [
+			{
+				name: "react",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "RESTapi",
+				color: "green-text-gradient",
+			},
+			{
+				name: "css",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "postgres",
+				color: "green-text-gradient",
+			},
+		],
+		image: wardrobe,
+		source_code_link: "https://gitlab.com/dangaritaho/microservice-two-shot",
+	},
+];
+
+export const projectsGitlab = [
+	{
+		name: "CaloriesPlanner",
+		description:
+		"CaloriesPlanner is an innovative food journal app designed to help you take control of your nutrition and achieve your health and fitness goals.",
+			// "CaloriesPlanner is an innovative food journal app designed to help you take control of your nutrition and achieve your health and fitness goals. With a focus on simplicity and accessibility, the app makes it easy for anyone to track their calories and stay on top of their nutrition goals, regardless of their level of expertise or experience. Whether you're looking to lose weight, gain muscle, or simply maintain a healthy lifestyle, CaloriesPlanner provides all the tools you need to succeed. With its intuitive interface and user-friendly features, the app is the perfect companion for anyone looking to improve their health and well-being. Start your journey towards better nutrition and a healthier you with CaloriesPlanner today.",
+		tags: [
+			{
+				name: "react",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "RESTapi",
+				color: "green-text-gradient",
+			},
+			{
+				name: "css",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "tailwind",
+				color: "green-text-gradient",
+			},
+			{
+				name: "mongodb",
+				color: "orange-text-gradient",
+			},
+			{
+				name: "redux toolkit",
+				color: "blue-text-gradient",
+			},
+		],
+		image: caloriesplanner,
+		source_code_link: "https://gitlab.com/dangaritaho/module3-project-gamma",
+	},
+];
